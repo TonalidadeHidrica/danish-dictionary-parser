b5a5b67cf1045fab4fd7e16bb8bfa2a33a5e19d8
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-08-06 02:01:32 +0900

    Read PDF file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use anyhow::Context;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Opts {
+    file: PathBuf,
+    page: u32,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts = Opts::parse();
+    let file = pdf::file::File::open(&opts.file)?;
+    let page = file.get_page(opts.page)?;
+    let contents = page
+        .contents
+        .as_ref()
+        .context("The page does not have contents")?;
+    for op in &contents.operations {
+        println!("{:?}", op);
+    }
+
+    Ok(())
 }
