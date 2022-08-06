8ca3d0eb5cb79072f953f4c7eb1879940c97c95b
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-08-07 03:15:02 +0900

    Use new version of PDF library

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use anyhow::Context;
 use clap::Parser;
-use danish_dictionary_parser::operator::Operator;
+// use danish_dictionary_parser::operator::Operator;
 
 use pdf::object::PageRc;
 
@@ -17,26 +17,23 @@ fn main() -> anyhow::Result<()> {
     let file = pdf::file::File::open(&opts.file)?;
 
     if let Some(page) = opts.page {
-        process_page(file.get_page(page)?)?;
+        process_page(&file, file.get_page(page)?)?;
     } else {
         for page in file.pages().flatten() {
-            process_page(page)?;
+            process_page(&file, page)?;
         }
     }
 
     Ok(())
 }
 
-fn process_page(page: PageRc) -> anyhow::Result<()> {
+fn process_page(file: &pdf::file::File<Vec<u8>>, page: PageRc) -> anyhow::Result<()> {
     let contents = page
         .contents
         .as_ref()
         .context("The page does not have contents")?;
-    for op in &contents.operations {
-        let _op: Operator = op
-            .clone()
-            .try_into()
-            .with_context(|| format!("While parsing {op:?}"))?;
+    for op in &contents.operations(file) {
+        println!("{:?}", op);
     }
     Ok(())
 }
